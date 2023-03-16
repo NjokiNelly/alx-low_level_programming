36de0a32201cfe0496b5c033e0d8c4c7abe56d10
Nelly <wambui>
2023-03-16 13:58:33 -0700

    4-Puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include>STDO.h
+/**
+ * main a program that prints a line with puts function
+ */
+int main [void]
+{
+	put ["/ programming is like building a multilingual puzzle /"] ;
+ return [o] ;
+}
