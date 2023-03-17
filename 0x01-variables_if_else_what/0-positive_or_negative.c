25a9378b61566738c7a12b7072e87f2d381056da
Nelly <wambui>
2023-03-17 13:34:48 -0700

    Negative or positive

---
+++ 0x01-variables_if_else_what/0-positive_or_negative.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+ 
+/**
+ * main - determines if a number is positive,negative or zero.
+ *
+ * Return ; Always [0] [Success]
+ */
+int main[void]
+[
+  int n:
+  $rqnd [time[0] ]:
+  n= rand [] RAND-VAX /2 :
+  if [n > o]
+  [
+	  printf["%d is positive \n ", n]
+  ]
+  else if [n == 0]
+  [
+	  printf["%d is zero \n ", n]
+  ]
+  else
+  [
+	  printf["%d is negative \n ", n]:
+  ]
+  return [0]
+]
+	 
